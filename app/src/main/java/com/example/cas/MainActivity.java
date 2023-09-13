9a155feacfa9094ffc448d8ddafe4c8575fdac81
PatandyaW <patandyawisnu@gmail.com>
2023-09-13 20:46:13 +0700

    button

--- app/src/main/java/com/example/cas/MainActivity.java
+++ app/src/main/java/com/example/cas/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.cas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+var nomorAntrian = 0
+        lateinit var tampilNomor: TextView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fTambah(View view) {
     }
 }
